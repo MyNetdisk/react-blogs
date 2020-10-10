a51f834b9692027fd63296943ac251e062ffe920
zhouzhihui <peregrinator@yeah.net>
2020-10-10 19:01:35 +0800

    chore: start docker service

--- deploy.sh
+++ deploy.sh
@@ -13,10 +13,17 @@ echo $docker_compose
 docker_compose_path=$project_path$docker_compose
 echo $docker_compose_path
 
+#数据库密码
+MYSQL_ROOT_PASSWORD="123456"
+#后台管理用户名
+ADMIN_USER_NAME="admin"
+#后台管理密码
+ADMIN_USER_PASSWORD="123456"
+
 #在此修改docker-compose.yml的环境变量：如mysql数据库管理员密码等敏感信息
-sed -i 's/$MYSQL_ROOT_PASSWORD/root/' $docker_compose_path
-sed -i 's/$ADMIN_USER_NAME/admin/' $docker_compose_path
-sed -i 's/$ADMIN_USER_PASSWORD/12345/' $docker_compose_path
+sed -i 's/$MYSQL_ROOT_PASSWORD/'$MYSQL_ROOT_PASSWORD'/' $docker_compose_path
+sed -i 's/$ADMIN_USER_NAME/'$ADMIN_USER_NAME'/' $docker_compose_path
+sed -i 's/$ADMIN_USER_PASSWORD/'$ADMIN_USER_PASSWORD'/' $docker_compose_path
 
 #停止所有容器
 docker stop $(docker ps -aq)
