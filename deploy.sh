36f7e03e3be2f3e47c29e2547056a7a190b42c36
zhouzhihui <peregrinator@yeah.net>
2020-10-10 18:52:03 +0800

    chore: start docker service

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,20 @@ set -e
 #此文件放在宿主机linux下任何目录下都可以 例如：/opt/docker/jenkins_shell/deploy.sh 同时加上可执行权限 chmod +x deploy.sh
 
 #查看当前目录
-project_path = $(pwd)
+#注意shell中定义变量等号两边没有空格 例如：project_path = $(pwd) 无效会报错
+project_path=$(pwd)
 echo $project_path
 #docker-compose.yml
-docker_compose = "/docker-compose.yml"
+docker_compose="/docker-compose.yml"
 echo $docker_compose
 #docker-compose.yml文件目录
-docker_compose_path = $project_path$docker_compose
+docker_compose_path=$project_path$docker_compose
 echo $docker_compose_path
 
 #在此修改docker-compose.yml的环境变量：如mysql数据库管理员密码等敏感信息
-sed -i 's/$MYSQL_ROOT_PASSWORD/root/' /var/lib/jenkins/workspace/Blogs/docker-compose.yml
-sed -i 's/$ADMIN_USER_NAME/admin/' /var/lib/jenkins/workspace/Blogs/docker-compose.yml
-sed -i 's/$ADMIN_USER_PASSWORD/12345/' /var/lib/jenkins/workspace/Blogs/docker-compose.yml
+sed -i 's/$MYSQL_ROOT_PASSWORD/root/' $docker_compose_path
+sed -i 's/$ADMIN_USER_NAME/admin/' $docker_compose_path
+sed -i 's/$ADMIN_USER_PASSWORD/12345/' $docker_compose_path
 
 #停止所有容器
 docker stop $(docker ps -aq)
